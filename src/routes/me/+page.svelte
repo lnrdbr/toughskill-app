44fc72f0c09ec28a5a58e4fb9be0102a43d61fd1
Leonard Bauer <leochriba@gmail.com>
2026-02-11 17:34:00 +0000

    Introduction

--- src/routes/me/+page.svelte
+++ src/routes/me/+page.svelte
@@ -1,11 +1,12 @@
 <script>
 	import LessonItem from "$lib/components/LessonItem.svelte";
-
+	import Cloudy from "$lib/assets/cloudy.png"
 </script>
 <h1 class="drop-shadow-lg drop-shadow-black/20">Creativity</h1>
 <div class="grid columns-2 grid-cols-[1fr_1.3fr] gap-3">
 <div class="w-full bg-secondary-100 rounded-2xl p-10 ">
-
+<div class="flex">
+						<div class="relative">
 <div class="relative size-52">
   <svg class="size-full -rotate-90 drop-shadow-lg" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
     <!-- Background Circle -->
@@ -19,7 +20,9 @@
     <span class="text-center text-4xl font-bold text-primary">35%</span>
   </div>
 </div>
-
+							 	</div>
+<img src={Cloudy} alt="Cloudy" class="w-full object-contain drop-shadow-md"/>
+</div>
 <div class="italic pt-5 font-light">"I'am proude of you! Keep doing What you do :) &#127882;"</div>
 </div>
 
